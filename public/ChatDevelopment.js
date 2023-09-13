268d75030b4d9da54bd7d24c9f995fb8254da0ba
Khanh Tran Van <tranvankhanh99a@gmail.com>
2023-09-13 14:46:07 +0700

    fix: add `Chat.js` for production

--- public/ChatDevelopment.js
+++ public/ChatDevelopment.js
@@ -9,7 +9,7 @@ function _0x5257() {
     'innerHTML',
     'push',
     'getReader',
-    'https://backend.dev.chatfly.co/api/chat/get-streaming-response',
+    'https://backend.chatfly.co/api/chat/get-streaming-response',
     '1010676mlyrEm',
     '7183757MifWDz',
     '<svg\x20fill=\x22#2a2b4c\x22\x20height=\x2229px\x22\x20width=\x2229px\x22\x20version=\x221.1\x22\x20id=\x22Capa_1\x22\x20xmlns=\x22http://www.w3.org/2000/svg\x22\x20xmlns:xlink=\x22http://www.w3.org/1999/xlink\x22\x20viewBox=\x22-6\x20-6\x2072.00\x2072.00\x22\x20xml:space=\x22preserve\x22\x20transform=\x22matrix(-1,\x200,\x200,\x201,\x200,\x200)\x22\x20stroke=\x22#9C1DE7\x22><g\x20id=\x22SVGRepo_bgCarrier\x22\x20stroke-width=\x220\x22></g><g\x20id=\x22SVGRepo_tracerCarrier\x22\x20stroke-linecap=\x22round\x22\x20stroke-linejoin=\x22round\x22\x20stroke=\x22#CCCCCC\x22\x20stroke-width=\x220.96\x22></g><g\x20id=\x22SVGRepo_iconCarrier\x22>\x20<path\x20d=\x22M30,1.5c-16.542,0-30,12.112-30,27c0,5.205,1.647,10.246,4.768,14.604c-0.591,6.537-2.175,11.39-4.475,13.689\x20c-0.304,0.304-0.38,0.769-0.188,1.153C0.276,58.289,0.625,58.5,1,58.5c0.046,0,0.093-0.003,0.14-0.01\x20c0.405-0.057,9.813-1.412,16.617-5.338C21.622,54.711,25.738,55.5,30,55.5c16.542,0,30-12.112,30-27S46.542,1.5,30,1.5z\x22></path>\x20</g></svg>',
---
+++ public/ChatProduction.js
@@ -0,0 +1,241 @@
+function _0x5257() {
+  const _0x52a397 = [
+    'onSendButton',
+    'decode',
+    '51802cXeJJR',
+    'createElement',
+    'add',
+    'body',
+    'innerHTML',
+    'push',
+    'getReader',
+    'https://backend.chatfly.co/api/chat/get-streaming-response',
+    '1010676mlyrEm',
+    '7183757MifWDz',
+    '<svg\x20fill=\x22#2a2b4c\x22\x20height=\x2229px\x22\x20width=\x2229px\x22\x20version=\x221.1\x22\x20id=\x22Capa_1\x22\x20xmlns=\x22http://www.w3.org/2000/svg\x22\x20xmlns:xlink=\x22http://www.w3.org/1999/xlink\x22\x20viewBox=\x22-6\x20-6\x2072.00\x2072.00\x22\x20xml:space=\x22preserve\x22\x20transform=\x22matrix(-1,\x200,\x200,\x201,\x200,\x200)\x22\x20stroke=\x22#9C1DE7\x22><g\x20id=\x22SVGRepo_bgCarrier\x22\x20stroke-width=\x220\x22></g><g\x20id=\x22SVGRepo_tracerCarrier\x22\x20stroke-linecap=\x22round\x22\x20stroke-linejoin=\x22round\x22\x20stroke=\x22#CCCCCC\x22\x20stroke-width=\x220.96\x22></g><g\x20id=\x22SVGRepo_iconCarrier\x22>\x20<path\x20d=\x22M30,1.5c-16.542,0-30,12.112-30,27c0,5.205,1.647,10.246,4.768,14.604c-0.591,6.537-2.175,11.39-4.475,13.689\x20c-0.304,0.304-0.38,0.769-0.188,1.153C0.276,58.289,0.625,58.5,1,58.5c0.046,0,0.093-0.003,0.14-0.01\x20c0.405-0.057,9.813-1.412,16.617-5.338C21.622,54.711,25.738,55.5,30,55.5c16.542,0,30-12.112,30-27S46.542,1.5,30,1.5z\x22></path>\x20</g></svg>',
+    'toggleIcon',
+    'updateChatBox',
+    'token',
+    'history',
+    'args',
+    'POST',
+    'Authorization',
+    'classList',
+    'forEach',
+    '<div\x20class=\x22messages__item\x20messages__item--typing\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<span\x20class=\x22messages__dot\x22></span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<span\x20class=\x22messages__dot\x22></span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<span\x20class=\x22messages__dot\x22></span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>',
+    'querySelector',
+    'input',
+    'Error:',
+    'addEventListener',
+    'URL',
+    'style',
+    '.chatbox__send--footer',
+    'state',
+    '1735WyDYen',
+    'chatbotConfig',
+    '38808EgdPau',
+    'slice',
+    'Bearer\x20',
+    '.chatbox__messages',
+    '<div>',
+    'click',
+    '10188zSbnKW',
+    '3507288UMshGg',
+    'showOrHideChatBox',
+    '8CsAXZK',
+    '<div\x20class=\x22messages__item\x20messages__item--operator\x22>',
+    'children',
+    'icons',
+    'status',
+    'sleep',
+    'display',
+    'Request\x20failed\x20with\x20status:\x20',
+    '.chatbox__support',
+    'head',
+    'toggleState',
+    '730QtnBYB',
+    'value',
+    'openButton',
+    '\x0a\x20\x20<link\x20href=\x22https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,600;1,300&display=swap\x22\x0a\x20\x20\x20\x20\x20\x20rel=\x22stylesheet\x22>\x0a\x20\x20<style>\x0a\x20\x20.chatbox__header,.chatbox__support,.messages__item--operator,.messages__item--typing,.messages__item--visitor{border-top-left-radius:20px;border-top-right-radius:20px}.chatbox{position:fixed;z-index:100000;bottom:30px;right:30px}.chatbox__support{display:flex;flex-direction:column;z-index:-123456;opacity:0;transition:.5s\x20ease-in-out}.chatbox--active{transform:translateY(-10px);z-index:123456;opacity:1}.chatbox__button{text-align:right}.chatbox__header{position:sticky;top:0;background:orange}.chatbox__messages{margin-top:auto;display:flex;flex-direction:column;overflow-y:scroll;flex-direction:column-reverse}.messages__item{width:fit-content}.messages__item--visitor{margin-right:auto}*{box-sizing:border-box;margin:0;padding:0}*,html{--primaryGradient:linear-gradient(93.12deg,\x20#581B98\x200.52%,\x20#9C1DE7\x20100%);--secondaryGradient:#2a2b4c;--primaryBoxShadow:0px\x2010px\x2015px\x20rgba(0,\x200,\x200,\x200.1);--secondaryBoxShadow:0px\x20-10px\x2015px\x20rgba(0,\x200,\x200,\x200.1);--light:300;--regular:400;--semiBold:600;--extraLight:300;--italic:300;--primary:#581B98}body{font-family:Nunito,sans-serif;font-weight:400;font-size:100%;background:#f1f1f1}.chatbox__support{border-bottom-left-radius:\x2020px;border-bottom-right-radius:\x2020px;background:#f9f9f9;height:450px;width:350px;box-shadow:0\x200\x2015px\x20rgba(0,0,0,.1)}.chatbox__header{background:var(--primaryGradient);display:flex;flex-direction:row;align-items:center;justify-content:center;padding:15px\x2020px;box-shadow:var(--primaryBoxShadow)}.chatbox__image--header{margin-right:10px}.chatbox__heading--header{font-size:1.2rem;color:#fff}.chatbox__description--header{font-size:.9rem;color:#fff}.chatbox__messages{height:100%;padding:0\x2020px}.messages__item{margin-top:10px;background:#e0e0e0;padding:8px\x2012px;max-width:70%}.messages__item--typing,.messages__item--visitor{border-bottom-right-radius:20px}.messages__item--operator{margin-left:auto;border-bottom-left-radius:20px;background:var(--primary);color:#fff}.chatbox__footer{position:sticky;bottom:0;display:flex;flex-direction:row;align-items:center;justify-content:space-between;padding:20px;background:var(--secondaryGradient);box-shadow:var(--secondaryBoxShadow);border-bottom-right-radius:10px;border-bottom-left-radius:10px;margin-top:20px}.chatbox__footer\x20input{border:none;outline:0;padding:10px;border-radius:30px;text-align:left;width:80%;height:80%}.chatbox__send--footer{color:#fff;cursor:pointer}.chatbox__button\x20button,.chatbox__button\x20button:focus,.chatbox__button\x20button:visited{padding:10px;background:#fff;border:none;outline:0;border-top-left-radius:50px;border-top-right-radius:50px;border-bottom-left-radius:50px;box-shadow:0\x2010px\x2015px\x20rgba(0,0,0,.1);cursor:pointer}.messages__item--typing{will-change:transform;width:auto;border-top-right-radius:20px;border-top-left-radius:20px;border-bottom-right-radius:20px;padding:15px\x2020px;display:table;margin-right:auto;position:relative;animation:2s\x20ease-out\x20infinite\x20bulge}.messages__item--typing::after,.messages__item--typing::before{content:\x27\x27;position:absolute;bottom:-2px;left:-2px;height:10px;width:10px;border-radius:50%}.messages__item--typing::after{height:10px;width:10px;left:-10px;bottom:-10px}span.messages__dot{height:8px;width:8px;float:left;margin:0\x201px;background-color:#9e9ea1;display:block;border-radius:50%;opacity:.4;animation:1s\x20infinite\x20blink}@keyframes\x20blink{50%{opacity:1}}@keyframes\x20bulge{50%{transform:scale(1.05)}}\x0a\x20\x20</style>\x0a',
+    '1038278bnsLAI',
+    'asyncGenerator',
+    'Enter',
+    'none',
+    '.chatbox__button',
+    'append',
+    'error',
+    'read',
+  ];
+  _0x5257 = function () {
+    return _0x52a397;
+  };
+  return _0x5257();
+}
+const _0xec467c = _0x25a2;
+(function (_0xc1fde5, _0x2f766c) {
+  const _0x2f6577 = _0x25a2,
+    _0x221e45 = _0xc1fde5();
+  while (!![]) {
+    try {
+      const _0x55d14e =
+        -parseInt(_0x2f6577(0xaa)) / 0x1 +
+        -parseInt(_0x2f6577(0xa0)) / 0x2 +
+        parseInt(_0x2f6577(0x8f)) / 0x3 +
+        (parseInt(_0x2f6577(0x8e)) / 0x4) * (-parseInt(_0x2f6577(0x86)) / 0x5) +
+        parseInt(_0x2f6577(0xb2)) / 0x6 +
+        (parseInt(_0x2f6577(0x72)) / 0x7) * (parseInt(_0x2f6577(0x91)) / 0x8) +
+        (-parseInt(_0x2f6577(0x88)) / 0x9) * (parseInt(_0x2f6577(0x9c)) / 0xa);
+      if (_0x55d14e === _0x2f766c) break;
+      else _0x221e45['push'](_0x221e45['shift']());
+    } catch (_0x1afa2b) {
+      _0x221e45['push'](_0x221e45['shift']());
+    }
+  }
+})(_0x5257, 0x9115b);
+class InteractiveChatbox {
+  constructor() {
+    const _0x415e79 = _0x25a2;
+    (this[_0x415e79(0x82)] = _0x415e79(0xb1)),
+      (this[_0x415e79(0x78)] = {
+        openButton: document['querySelector'](_0x415e79(0xa4)),
+        chatbox: document[_0x415e79(0x7e)](_0x415e79(0x99)),
+        sendButton: document[_0x415e79(0x7e)](_0x415e79(0x84)),
+      }),
+      (this['icons'] = {
+        isClicked: _0x415e79(0x73),
+        isNotClicked: _0x415e79(0x73),
+      }),
+      (this['state'] = ![]),
+      (this['history'] = []);
+  }
+  async [_0xec467c(0x97)]() {
+    const _0x5b9688 = _0xec467c,
+      {
+        openButton: _0x5ae134,
+        chatbox: _0x4d27f1,
+        sendButton: _0x73447f,
+      } = this[_0x5b9688(0x78)];
+    this[_0x5b9688(0x74)](![], _0x5ae134),
+      this[_0x5b9688(0x9b)](_0x4d27f1),
+      _0x5ae134['addEventListener']('click', () =>
+        this[_0x5b9688(0x9b)](_0x4d27f1),
+      ),
+      _0x73447f[_0x5b9688(0x81)](_0x5b9688(0x8d), () =>
+        this[_0x5b9688(0xa8)](_0x4d27f1),
+      );
+    const _0x40f2d3 = _0x4d27f1[_0x5b9688(0x7e)](_0x5b9688(0x7f));
+    _0x40f2d3[_0x5b9688(0x81)]('keyup', ({ key: _0x171178 }) => {
+      const _0x486b3e = _0x5b9688;
+      _0x171178 === _0x486b3e(0xa2) && this[_0x486b3e(0xa8)](_0x4d27f1);
+    });
+  }
+  [_0xec467c(0x96)](_0x593409) {
+    return new Promise((_0x1964fb) => setTimeout(_0x1964fb, _0x593409));
+  }
+  async ['onSendButton'](_0x1a9f97) {
+    const _0x337750 = _0xec467c;
+    let _0x11a527 = _0x1a9f97[_0x337750(0x7e)]('input'),
+      _0x14186a = _0x11a527[_0x337750(0x9d)];
+    _0x11a527[_0x337750(0x9d)] = '';
+    if (_0x14186a === '') return;
+    this['history'][_0x337750(0xaf)]([_0x14186a, '']), this[_0x337750(0x75)]();
+    let _0x73d418 = new Headers();
+    _0x73d418[_0x337750(0xa5)]('Content-Type', 'application/json'),
+      _0x73d418[_0x337750(0xa5)](
+        _0x337750(0x7a),
+        _0x337750(0x8a) + window[_0x337750(0x87)][_0x337750(0x76)],
+      );
+    const _0x5c6538 = {
+      method: _0x337750(0x79),
+      headers: _0x73d418,
+      body: JSON['stringify']({
+        message: _0x14186a,
+        ...window[_0x337750(0x87)],
+      }),
+    };
+    try {
+      const _0x5d4a3a = await fetch(this[_0x337750(0x82)], _0x5c6538);
+      if (!_0x5d4a3a['ok'])
+        throw new Error(_0x337750(0x98) + _0x5d4a3a[_0x337750(0x95)]);
+      const _0x3b0bf1 = await _0x5d4a3a['body'],
+        _0x3396d1 = _0x3b0bf1[_0x337750(0xb0)]();
+      for await (const _0x1fcb7b of this[_0x337750(0xa1)](_0x3396d1)) {
+      }
+    } catch (_0x27028e) {
+      console[_0x337750(0xa6)](_0x337750(0x80), _0x27028e),
+        (_0x11a527[_0x337750(0x9d)] = '');
+    }
+  }
+  async *['asyncGenerator'](_0x3c0235) {
+    const _0x56f144 = _0xec467c,
+      _0x123004 = new TextDecoder();
+    while (!![]) {
+      const { done: _0x16b396, value: _0x40bcab } =
+        await _0x3c0235[_0x56f144(0xa7)]();
+      if (_0x16b396) return;
+      const _0x3d86ce = _0x123004[_0x56f144(0xa9)](_0x40bcab);
+      await this['sleep'](0x32),
+        (this['history'][_0x56f144(0x89)](-0x1)[0x0][0x1] += _0x3d86ce),
+        await this[_0x56f144(0x75)]();
+    }
+  }
+  async ['updateChatBox']() {
+    const _0x47ce97 = _0xec467c;
+    let _0x4cfc60 = document[_0x47ce97(0x7e)](_0x47ce97(0x8b)),
+      _0x87ee9f = '';
+    this[_0x47ce97(0x77)][_0x47ce97(0x7c)]((_0x311038, _0x394219) => {
+      const _0xf89358 = _0x47ce97;
+      _0x87ee9f += _0xf89358(0x92) + _0x311038[0x0] + '</div>';
+      if (_0x311038[0x1])
+        _0x87ee9f +=
+          '<div\x20class=\x22messages__item\x20messages__item--visitor\x22>' +
+          _0x311038[0x1] +
+          '</div>';
+      else _0x87ee9f += _0xf89358(0x7d);
+    }),
+      (_0x4cfc60['innerHTML'] = _0x47ce97(0x8c) + _0x87ee9f + '</div>');
+  }
+  [_0xec467c(0x9b)](_0x1f331d) {
+    const _0x14a62c = _0xec467c;
+    (this['state'] = !this[_0x14a62c(0x85)]),
+      this[_0x14a62c(0x90)](_0x1f331d, this[_0x14a62c(0x78)][_0x14a62c(0x9e)]);
+  }
+  [_0xec467c(0x90)](_0x3ae226, _0x1102bc) {
+    const _0x25d4ae = _0xec467c;
+    _0x3ae226[_0x25d4ae(0x83)][_0x25d4ae(0x97)] =
+      _0x3ae226[_0x25d4ae(0x83)]['display'] === _0x25d4ae(0xa3)
+        ? ''
+        : _0x25d4ae(0xa3);
+    if (this[_0x25d4ae(0x85)])
+      _0x3ae226[_0x25d4ae(0x7b)][_0x25d4ae(0xac)]('chatbox--active'),
+        this[_0x25d4ae(0x74)](!![], _0x1102bc);
+    else !this[_0x25d4ae(0x85)] && this[_0x25d4ae(0x74)](![], _0x1102bc);
+  }
+  [_0xec467c(0x74)](_0x10c806, _0x4466bb) {
+    const _0x3b2ef9 = _0xec467c,
+      { isClicked: _0x4b96d8, isNotClicked: _0x32a92d } = this[_0x3b2ef9(0x94)];
+    let _0x1f9410 = _0x4466bb[_0x3b2ef9(0x93)][0x0][_0x3b2ef9(0xae)];
+    if (_0x10c806) _0x4466bb[_0x3b2ef9(0x93)][0x0][_0x3b2ef9(0xae)] = _0x4b96d8;
+    else
+      !_0x10c806 &&
+        (_0x4466bb[_0x3b2ef9(0x93)][0x0][_0x3b2ef9(0xae)] = _0x32a92d);
+  }
+}
+(document['body'] = document[_0xec467c(0xab)]('body')),
+  (document[_0xec467c(0x9a)][_0xec467c(0xae)] += _0xec467c(0x9f));
+const elemDiv =
+  '\x0a\x20\x20<div\x20class=\x22container\x22>\x0a\x20\x20\x20\x20\x20\x20<div\x20class=\x22chatbox\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22chatbox__support\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22chatbox__messages\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22chatbox__footer\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<input\x20type=\x22text\x22\x20placeholder=\x22Write\x20a\x20message...\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p\x20class=\x22chatbox__send--footer\x22>Send</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22chatbox__button\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<button>button</button>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20</div>\x0a\x20\x20';
+document[_0xec467c(0xad)][_0xec467c(0xae)] += elemDiv;
+function _0x25a2(_0x25df53, _0x29c6a9) {
+  const _0x5257c2 = _0x5257();
+  return (
+    (_0x25a2 = function (_0x25a286, _0x16e336) {
+      _0x25a286 = _0x25a286 - 0x72;
+      let _0x57e051 = _0x5257c2[_0x25a286];
+      return _0x57e051;
+    }),
+    _0x25a2(_0x25df53, _0x29c6a9)
+  );
+}
+const chatbox = new InteractiveChatbox();
+chatbox[_0xec467c(0x97)]();
