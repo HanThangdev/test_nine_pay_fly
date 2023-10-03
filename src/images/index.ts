cee636b87b97a8a4126cb12af4259c5bf69ddadc
hientt <78089627+Hien1207@users.noreply.github.com>
2023-10-03 21:40:41 +0700

    [CF] : add button integration (#49)
    * CF : add button integration
    
    * CF : fix icon zalo

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,5 +1,5 @@
 import backgroundRegisterPricing from './backgroundRegisterPricing.png';
 import iconWordpress from './icon_Wordpress.png';
-import iconZalo from './icon_Zalo.png';
+import iconZalo from './icon_zalo.png';
 
 export { backgroundRegisterPricing, iconWordpress, iconZalo };
